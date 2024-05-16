b2c54a2b48ff6c36d9d8f41b30f1f6657bac2106
Erik Henningson <ErikHen@users.noreply.github.com>
2024-05-16 14:56:04 +0200

    Typed contentreferencelist

--- OptiContentClient/Models/Properties/PropertyContentReferenceList.cs
+++ OptiContentClient/Models/Properties/PropertyContentReferenceList.cs
@@ -1,11 +1,15 @@
 ﻿
-using System.Linq;
-
 namespace OptiContentClient.Models.Properties
 {
     public class PropertyContentReferenceList : PropertyBase
     {
         public ContentLink[]? Value { get; set; }
         public Content[]? ExpandedValue { get; set; }
+    }
+
+    public class PropertyContentReferenceList<T> : PropertyBase where T : Content
+    {
+        public ContentLink[]? Value { get; set; }
+        public T[]? ExpandedValue { get; set; }
     }
 }
